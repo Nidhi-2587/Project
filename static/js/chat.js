27078032207a4c5d34f446295714c2ad6aea2141
BanuTej007 <144442282+BanuTej007@users.noreply.github.com>
2023-10-05 21:48:11 +0530

    Create chat.js

---
+++ static/js/chat.js
@@ -0,0 +1,125 @@
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+// Gets the first message
+function firstBotMessage() {
+    let firstMessage = "How can I help you?"
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
+
+// Function to send a POST request to the server
+function sendMessage() {
+    const userInput = document.getElementById("textInput").value;
+    
+    if (userInput.trim() === "") {
+        return;
+    }
+
+    const requestData = {
+        message: userInput
+    };
+
+    fetch("/api/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        displayMessage(data.response);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+
+    // Clear the input field
+    document.getElementById("textInput").value = "";
+
+    let userHtml = '<p class="userText"><span>' + userInput + '</span></p>';
+    
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+// Function to display a user or chatbot message in the chatbox
+function displayMessage(message) {
+    const chatbox = document.getElementById("chatbox");
+    const messageElement = document.createElement("div");
+    messageElement.className = "message";
+
+    messageElement.innerHTML = `<p class="botText"><span>${message}</span></p>`;
+
+    chatbox.appendChild(messageElement);
+
+    // Scroll to the bottom of the chatbox to show the latest message
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+// Event listener for the Enter key
+document.getElementById("textInput").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
+
+// Event listener for the send button
+document.querySelector(".fa-send").addEventListener("click", function () {
+    sendMessage();
+});
+
+// Initial message when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    sendMessage();
+});
+
+function heartButton() {
+    const userInput = "Heart clicked!";
+
+    let userHtml = '<p class="userText"><span>' + userInput + '</span></p>';
+    
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
