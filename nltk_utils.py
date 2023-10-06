8cf92896e4eb44ea74840833655fa78702a2de2c
Nidhi-2587 <114771815+Nidhi-2587@users.noreply.github.com>
2023-10-06 16:58:10 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -13,9 +13,6 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    #sentence = ["hello","how","are","you"]
-    #words=["hi","hello","I","you","bye","thank","cool"] #patterns
-    #bag = [  0,    1,    0,   1,    0,    0,      0]
     
     tokenized_sentence= [stem(w) for w in tokenized_sentence]
     
@@ -26,23 +23,4 @@ def bag_of_words(tokenized_sentence, all_words):
     return bag
 
 
-
-# Now you can use 'bag_input' as input to your trained model to predict the intent.
-
-#bag_of_words
-#sentence = ["hello","how","are","you"]
-#words=["hi","hello","I","you","bye","thank","cool"]
-#bag = bag_of_words(sentence,words)
-#print(bag)
-
-#stemming of words
-#words = ["Organize","organizes","Organizing"]
-#stemmed_words = [stem(w) for w in words]
-#print(stemmed_words)
-#a= "Which items do you sell?"
-
-#tokenizing of words
-
-#a = tokenize(a)
-#print(a)
 
